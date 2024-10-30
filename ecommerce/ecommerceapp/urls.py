2fd2fd750c848da71ed13c4da9c68d525b33c84d
sakib387 <jucse28.387>
2024-10-30 15:04:13 +0600

    nav added

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from ecommerceapp import views
 
 urlpatterns = [
-    path('', views.index,name="index")
+    path('', views.index,name="index"),
+       path('contact', views.contact,name="contact"),
+       path('about', views.about,name="about")
 ]
