00b4e8900e0d3377328b75fe87a39620a2e980ff
sakib387 <jucse28.387>
2024-11-03 16:08:08 +0600

    product model and add product and display it

--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from ecommerceapp.models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    all_products = []
+    cat_prods = Product.objects.values('category', 'id')
+    categories = {item['category'] for item in cat_prods}
+
+    for category in categories:
+        products = Product.objects.filter(category=category)
+        n = len(products)
+        slides = n // 4 + ceil((n / 4) - (n // 4))
+        all_products.append({'category': category, 'products': products, 'slides': slides})
+
+    params = {'all_products': all_products}
+    return render(request, "index.html", params)
 
 def contact(request):
     return render(request,"contact.html")
